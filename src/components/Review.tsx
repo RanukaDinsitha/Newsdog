81c363f5954385c916584f7d0ca4b9c43c0e1060
RanukaDinsitha <ranuka@outlook.co.nz>
2024-08-20 20:54:15 +1200

    I am, not using Tailwind.

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -1,7 +1,48 @@
-import Typeform from "./Typeform.tsx";
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import IconButton from '@mui/material/IconButton';
+import { X, NotebookPen } from 'lucide-react';
+import Typeform from './Typeform';
+import './Review.css';
 
 const TransparentReviewButton = () => {
-  return <Typeform />;
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button
+        variant="text"
+        startIcon={<NotebookPen className="icon" size={24}/>}
+        className="review-button"
+        onClick={handleClickOpen}
+      >
+        <div className="text-box">
+          Review
+        </div>
+      </Button>
+      <Dialog open={open} onClose={handleClose} maxWidth="lg">
+        <div className="modal-content">
+          <IconButton className="close-icon" onClick={handleClose}>
+            <X size={24}/>
+          </IconButton>
+          <div className="typeform-box">
+            <div className="container">
+              <Typeform />
+            </div>
+          </div>
+        </div>
+      </Dialog>
+    </div>
+  );
 };
 
 export default TransparentReviewButton;
--- src/components/Typeform.tsx
+++ src/components/Typeform.tsx
@@ -1,18 +1,8 @@
-import { PopupButton } from "@typeform/embed-react";
-import { NotebookPen } from "lucide-react";
+import { Widget } from "@typeform/embed-react";
 import "./Typeform.css";
 
 const Typeform = () => {
-  return (
-    <PopupButton
-      id="https://gyd6ayvs92d.typeform.com/to/REH1jRvn"
-      style={{ fontSize: 18 }}
-      className="typeform-button"
-    >
-      <NotebookPen size={24} className="notebook-icon"></NotebookPen>
-      <p className="review-text">Review</p>
-    </PopupButton>
-  );
+  return <Widget id="https://gyd6ayvs92d.typeform.com/to/REH1jRvn" style={{ width: '50%' }} className="typeform" />
 };
 
 export default Typeform;
